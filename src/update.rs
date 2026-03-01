cd786a286d94bc64343bbea0d11c0e1ba48acf94
CultOfTheBlob <human.choupani@gmail.com>
2026-03-02 01:23:18 +0330

    Fixed a bug when automatically selecting created project

--- src/update.rs
+++ src/update.rs
@@ -104,7 +104,7 @@ pub fn update(state: &mut AppState, message: Message) -> Task<Message>
             }
 
             state.new_project = Project::default(&state.config);
-            state.selected_project = Some(state.project_list.len() + 1);
+            state.selected_project = Some(state.project_list.len() - 1);
             state.pending = None;
             state.new_project_path_changed = false;
 
