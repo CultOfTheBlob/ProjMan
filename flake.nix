2bdc2625ce50713ccb88dcbfec9eed21cc4fdabe
CultOfTheBlob <human.choupani@gmail.com>
2026-03-30 17:56:43 +0330

    Updated nix flake to be a usable input
    Im not sure if this works yet, but I need to push these changes to try
    it out

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description = "Rust development enviroment.";
-
+  description = "Rust development environment.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
@@ -10,72 +9,68 @@
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
+    self,
     nixpkgs,
     naersk,
     rust-overlay,
+    flake-utils,
     ...
-  }: let
-    system = "x86_64-linux";
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+        naerskLib = pkgs.callPackage naersk {};
+      in {
+        packages.default = naerskLib.buildPackage {
+          src = ./.;
+          buildInputs = with pkgs; [glib openssl];
+          nativeBuildInputs = with pkgs; [pkg-config makeWrapper];
+          OPENSSL_NO_VENDOR = 1;
 
-    overlays = [(import rust-overlay)];
+          postInstall = ''
+            wrapProgram $out/bin/projman \
+              --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath (with pkgs; [
+              wayland
+              libxkbcommon
+              libGL
+              mesa
+              vulkan-loader
+            ])}
+          '';
+        };
 
-    pkgs = import nixpkgs {
-      inherit system overlays;
-    };
-    naerskLib = pkgs.callPackage naersk {};
-  in {
-    packages."x86_64-linux".default = naerskLib.buildPackage {
-      src = ./.;
-
-      buildInputs = with pkgs; [
-        glib
-        openssl
-      ];
-      nativeBuildInputs = [pkgs.pkg-config];
-    };
-
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        (
-          rust-bin.selectLatestNightlyWith (toolchain:
-            toolchain.default.override {
-              extensions = [
-                "rust-src"
-                "rust-analyzer"
-                "rustfmt"
-              ];
-            })
-        )
-
-        cargo
-        clippy
-        glib
-        openssl.dev
-      ];
-
-      LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-        pkgs.wayland
-        pkgs.libxkbcommon
-        pkgs.libGL
-        pkgs.mesa
-        pkgs.vulkan-loader
-      ];
-
-      nativeBuildInputs = [pkgs.pkg-config];
-
-      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-      shellHook = ''
-        ${pkgs.onefetch}/bin/onefetch
-
-        alias run="cargo run"
-        alias add="cargo add"
-        alias build="nix build"
-        alias nv="nvim -u /home/blob/.config/nvim/init.lua"
-      '';
-    };
-  };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (rust-bin.selectLatestNightlyWith (toolchain:
+              toolchain.default.override {
+                extensions = ["rust-src" "rust-analyzer" "rustfmt"];
+              }))
+            cargo
+            clippy
+            glib
+            openssl.dev
+          ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.wayland
+            pkgs.libxkbcommon
+            pkgs.libGL
+            pkgs.mesa
+            pkgs.vulkan-loader
+          ];
+          nativeBuildInputs = [pkgs.pkg-config];
+          env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          shellHook = ''
+            alias run="cargo run"
+            alias add="cargo add"
+            alias build="nix build"
+          '';
+        };
+      }
+    );
 }
