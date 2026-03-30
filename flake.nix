c8789c3a4928725437cfe455fe91ab6c38b57c4d
CultOfTheBlob <human.choupani@gmail.com>
2026-03-30 20:03:49 +0330

    Added a desktop entry to flake.nix
    This will make the app show up with a custom icon in launchers like
    rofi.

--- flake.nix
+++ flake.nix
@@ -33,8 +33,18 @@
           nativeBuildInputs = with pkgs; [pkg-config makeWrapper];
           OPENSSL_NO_VENDOR = 1;
 
-          postInstall = ''
-            wrapProgram $out/bin/projman \
+          postInstall = let
+            desktopEntry = pkgs.lib.generators.toINI {} {
+              "Desktop Entry" = {
+                Name = "ProjMan";
+                Exec = "${placeholder "out"}/bin/projman";
+                Icon = "projman";
+                Type = "Application";
+                Categories = "Utility;";
+              };
+            };
+          in ''
+              wrapProgram $out/bin/projman \
               --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath (with pkgs; [
               wayland
               libxkbcommon
@@ -42,6 +52,11 @@
               mesa
               vulkan-loader
             ])}
+
+            install -Dm644 ${./assets/icon.svg} $out/share/icons/hicolor/scalable/apps/projman.svg
+
+            mkdir -p $out/share/applications
+            echo "${desktopEntry}" > $out/share/applications/projman.desktop
           '';
         };
 
