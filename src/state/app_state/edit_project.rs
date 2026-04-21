ab149069756b1fcc0cd65629439b695d93c485d9
CultOfTheBlob <human.choupani@gmail.com>
2026-04-22 00:51:10 +0330

    Fixed a bug when editing a project
    AppState::edit_project did not update the contents in the projects
    projman.toml file. I didnt notice this before because the projman file
    wasnt compared to the projects information in projects.json to check the
    projects validity.

--- src/state/app_state/edit_project.rs
+++ src/state/app_state/edit_project.rs
@@ -1,6 +1,6 @@
 use crate::{
     error::{Error, ErrorInfo},
-    project::Project,
+    project::{Project, ProjmanFile},
     state::app_state::AppState,
 };
 use std::fs;
@@ -36,8 +36,44 @@ impl AppState
 
         if let Some(index) = self.selected_project
         {
-            projects[index].name = self.edit_project_name.clone();
-            projects[index].repo = self.edit_project_repo.clone();
+            let project = &mut projects[index];
+
+            project.name = self.edit_project_name.clone();
+            project.repo = self.edit_project_repo.clone();
+
+            let projman_path = project.path.join(ProjmanFile::FILE_NAME);
+
+            let projman_file_from_toml = match &fs::read_to_string(&projman_path)
+            {
+                Ok(string) => string.clone(),
+                Err(err) =>
+                {
+                    return Err(error!(Error::Read, ProjmanFile::FILE_NAME, err));
+                }
+            };
+
+            let mut projman_file: ProjmanFile = match toml::from_str(&projman_file_from_toml)
+            {
+                Ok(projman_file) => projman_file,
+                Err(err) => return Err(error!(Error::Parse, ProjmanFile::FILE_NAME, err)),
+            };
+
+            projman_file.name = self.edit_project_name.clone();
+            projman_file.repo = self.edit_project_repo.clone();
+
+            let projman_file_to_toml = match toml::to_string_pretty(&projman_file)
+            {
+                Ok(json) => json,
+                Err(err) =>
+                {
+                    return Err(error!(Error::Parse, ProjmanFile::FILE_NAME, err));
+                }
+            };
+
+            if let Err(err) = fs::write(&projman_path, projman_file_to_toml.as_bytes())
+            {
+                return Err(error!(Error::Write, ProjmanFile::FILE_NAME, err));
+            }
         }
 
         let projects_to_json = match serde_json::to_string_pretty(&projects)
